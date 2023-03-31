da859b9dabb05bc3847f20a6d73bd1610f950299
AustereTriceratops <gatewaylevi@gmail.com>
2023-03-31 02:27:37 -0500

    most of multi-head attention written

--- bert/attention.py
+++ bert/attention.py
@@ -4,18 +4,17 @@ import torch.nn as nn
 import numpy as np
 
 class Attention(nn.Module):
-    def __init__(self, input_channels, output_channels=None, hidden_channels=None, dropout_rate=0.1) -> None:
+    def __init__(self, in_channels, out_channels=None, hidden_channels=None, dropout_rate=0.1) -> None:
         super().__init__()
 
-        if not output_channels:
-            output_channels = input_channels
+        if not out_channels:
+            out_channels = in_channels
         if not hidden_channels:
-            hidden_channels = input_channels
+            hidden_channels = in_channels
 
-
-        self.Q_projection = nn.Conv1d(kernel_size=1, in_channels=input_channels, out_channels=hidden_channels)
-        self.K_projection = nn.Conv1d(kernel_size=1, in_channels=input_channels, out_channels=hidden_channels)
-        self.V_projection = nn.Conv1d(kernel_size=1, in_channels=input_channels, out_channels=output_channels)
+        self.Q_projection = nn.Conv1d(kernel_size=1, in_channels=in_channels, out_channels=hidden_channels)
+        self.K_projection = nn.Conv1d(kernel_size=1, in_channels=in_channels, out_channels=hidden_channels)
+        self.V_projection = nn.Conv1d(kernel_size=1, in_channels=in_channels, out_channels=out_channels)
 
         # softmax will be applied over the channels
         self.softmax = nn.Softmax(2)
@@ -32,6 +31,7 @@ class Attention(nn.Module):
         '''
         x = x.transpose(1, 2)
 
+        # TODO: these should just be linear layers
         Q = self.Q_projection( x ).transpose(1, 2)
         K = self.K_projection( x ).transpose(1, 2)
         V = self.V_projection( x ).transpose(1, 2)
@@ -52,26 +52,69 @@ class Attention(nn.Module):
 
         x = torch.matmul(Q, torch.transpose(K, 1, 2)) / np.sqrt(d)
         A = self.dropout(self.softmax(x))
-        out = torch.matmul(x, V)
+                         
+        out = torch.matmul(A, V)
 
         return out
 
 
+# TODO: write tests
+# TODO: return values of docstrings
+# TODO: Move dot product attention out to its own class
 class MultiheadAttention(Attention):
-    def __init__(self, channels_V, channels_out) -> None:
+    def __init__(self, in_channels, out_channels=None, num_heads=1, hidden_channels=None, dropout_rate=0.1) -> None:
         super().__init__()
 
-        self.linear = nn.Conv1d(kernel_size=1, in_channels=channels_V, out_channels=channels_out)
+        if not out_channels:
+            out_channels = in_channels
+        if not hidden_channels:
+            hidden_channels = in_channels
 
-    def forward(self, Q, K, V) -> torch.Tensor:
+        self.total_hidden_channels = num_heads * hidden_channels
+        self.total_out_channels = num_heads * out_channels
+        self.num_heads = num_heads
+        self.hidden_channels = hidden_channels
+        
+        self.Q_projection = nn.Linear(in_channels=in_channels, out_channels=self.total_hidden_channels)
+        self.K_projection = nn.Linear(in_channels=in_channels, out_channels=self.total_hidden_channels)
+        self.V_projection = nn.Linear(in_channels=in_channels, out_channels=self.total_out_channels)
+
+        self.softmax = nn.Softmax(3)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def dot_product_attention(self, Q, K, V) -> torch.Tensor:
+        '''
+        Dot-product attention 
+
+        K: a tensor with shape (batch, heads, length, hidden_channels) 
+        Q: a tensor with shape (batch, heads, length, hidden_channels) 
+        V: a tensor with shape (batch, heads, length, output_channels)
+
+        returns a tensor with shape (batch, heads, length, output_channels)
+        '''
+        d = Q.shape[-1]
+
+        x = torch.matmul(Q, torch.transpose(K, 2, 3)) / np.sqrt(d)
+        A = self.dropout(self.softmax(x))
+                         
+        out = torch.matmul(A, V)
+
+        return out
+
+    def forward(self, x) -> torch.Tensor:
         '''
         Multi-head attention attention 
 
-        Q: a tensor with shape (batch, length, channels) 
-        K: a tensor with shape (batch, length, channels) 
-        V: a tensor with shape (batch, length, channels_) 
-
-        TODO: implement
-        TODO: make compatible with dropout
+        x: a tensor with shape (batch, length, channels) 
         '''
-        pass
+        batch_size = x.shape[0]
+        seq_len = x.shape[1]
+
+        Q = self.Q_projection(x).reshape(batch_size, seq_len, self.num_heads, self.hidden_channels).permute(0, 2, 1, 3)
+        K = self.K_projection(x).reshape(batch_size, seq_len, self.num_heads, self.hidden_channels).permute(0, 2, 1, 3)
+        V = self.V_projection(x).reshape(batch_size, seq_len, self.num_heads, self.output_channels).permute(0, 2, 1, 3)
+
+        out = self.dot_product_attention(Q, K, V)
+        out = out.permute(0, 2, 1, 3).reshape(batch_size, seq_len, self.total_out_channels)
+
+        return out
